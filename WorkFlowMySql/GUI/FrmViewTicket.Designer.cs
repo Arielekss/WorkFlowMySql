7a7a42358f218c91bb522d649c4e346d15aa4f27
Arielekss <ariel.kazmierczak@gmail.com>
2017-10-23 21:17:04 +0200

    23.10
    23.10

--- WorkFlowMySql/GUI/FrmViewTicket.Designer.cs
+++ WorkFlowMySql/GUI/FrmViewTicket.Designer.cs
@@ -28,47 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.dgTicketView = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgTicketView)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 233);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(563, 51);
-            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // dgTicketView
             // 
             this.dgTicketView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dgTicketView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.dgTicketView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgTicketView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgTicketView.Dock = System.Windows.Forms.DockStyle.Top;
             this.dgTicketView.Location = new System.Drawing.Point(0, 0);
             this.dgTicketView.Name = "dgTicketView";
-            this.dgTicketView.Size = new System.Drawing.Size(563, 233);
+            this.dgTicketView.Size = new System.Drawing.Size(563, 216);
             this.dgTicketView.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 222);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(563, 62);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(476, 27);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
             // FrmViewTicket
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
             this.ClientSize = new System.Drawing.Size(563, 284);
             this.Controls.Add(this.dgTicketView);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
             this.Name = "FrmViewTicket";
             this.Text = "FrmViewTicket";
             ((System.ComponentModel.ISupportInitialize)(this.dgTicketView)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.DataGridView dgTicketView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClose;
     }
 }
