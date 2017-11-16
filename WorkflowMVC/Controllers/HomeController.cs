7e66ad24ea2cd1f92c82e2d7e3356f584a6ff39f
Arielekss <ariel.kazmierczak@gmail.com>
2017-11-16 22:44:39 +0100

    Add MVC

---
+++ WorkflowMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkFlowMySql.Data;
+
+namespace WorkflowMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        TicketModel ticketModel = new TicketModel();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult TicketCreator()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult CreateTicket()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+    }
+}
