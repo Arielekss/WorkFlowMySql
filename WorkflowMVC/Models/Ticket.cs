185e5cb7afd5a3718efaa0c487efe22d4c58ce92
Arielekss <ariel.kazmierczak@gmail.com>
2017-11-16 23:01:54 +0100

    16.11 [2]

---
+++ WorkflowMVC/Models/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorkflowMVC.Models
+{
+    public class Ticket
+    {
+            [Key]
+            public int TicketId { get; set; }
+            public string Header { get; set; }
+            public string Content { get; set; }
+            public string UserRegister { get; set; }
+            public string ActiveUser { get; set; }
+            public DateTime RegisterDate { get; set; }
+            public DateTime? CloseDate { get; set; }
+            public string Priority { get; set; }
+            public DateTime Deadline { get; set; }
+        
+    }
+}
