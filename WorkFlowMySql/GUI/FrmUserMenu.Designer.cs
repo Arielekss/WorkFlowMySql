beca61e2f71cf7ed909890caaaaac2457c12ef4e
Arielekss <ariel.kazmierczak@gmail.com>
2017-12-02 17:46:14 +0100

    Add User Menu

--- WorkFlowMySql/GUI/FrmUserMenu.Designer.cs
+++ WorkFlowMySql/GUI/FrmUserMenu.Designer.cs
@@ -34,11 +34,19 @@
             this.btnTicketView = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
             this.btnUserTick = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(50, 9);
+            this.txtUser.Location = new System.Drawing.Point(44, 27);
             this.txtUser.Name = "txtUser";
             this.txtUser.ReadOnly = true;
             this.txtUser.Size = new System.Drawing.Size(100, 20);
@@ -47,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(6, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 1;
@@ -56,7 +64,7 @@
             // btnCreateTick
             // 
             this.btnCreateTick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCreateTick.Location = new System.Drawing.Point(9, 94);
+            this.btnCreateTick.Location = new System.Drawing.Point(75, 53);
             this.btnCreateTick.Name = "btnCreateTick";
             this.btnCreateTick.Size = new System.Drawing.Size(86, 23);
             this.btnCreateTick.TabIndex = 2;
@@ -67,7 +75,7 @@
             // btnTicketView
             // 
             this.btnTicketView.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnTicketView.Location = new System.Drawing.Point(182, 94);
+            this.btnTicketView.Location = new System.Drawing.Point(248, 53);
             this.btnTicketView.Name = "btnTicketView";
             this.btnTicketView.Size = new System.Drawing.Size(75, 23);
             this.btnTicketView.TabIndex = 3;
@@ -78,7 +86,7 @@
             // btnLogout
             // 
             this.btnLogout.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnLogout.Location = new System.Drawing.Point(181, 7);
+            this.btnLogout.Location = new System.Drawing.Point(175, 25);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(75, 23);
             this.btnLogout.TabIndex = 4;
@@ -88,7 +96,7 @@
             // btnUserTick
             // 
             this.btnUserTick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnUserTick.Location = new System.Drawing.Point(101, 94);
+            this.btnUserTick.Location = new System.Drawing.Point(167, 53);
             this.btnUserTick.Name = "btnUserTick";
             this.btnUserTick.Size = new System.Drawing.Size(75, 23);
             this.btnUserTick.TabIndex = 5;
@@ -96,21 +104,66 @@
             this.btnUserTick.UseVisualStyleBackColor = true;
             this.btnUserTick.Click += new System.EventHandler(this.btnUserTick_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUserTick);
+            this.groupBox1.Controls.Add(this.btnCreateTick);
+            this.groupBox1.Controls.Add(this.btnTicketView);
+            this.groupBox1.Location = new System.Drawing.Point(318, 346);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(336, 91);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Service actions";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtUser);
+            this.groupBox2.Controls.Add(this.btnLogout);
+            this.groupBox2.Location = new System.Drawing.Point(378, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(263, 70);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Active user";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(1, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(664, 252);
+            this.panel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::WorkFlowMySql.Properties.Resources.main_qimg_0a4a89cb00ef290cf45c1a0e468a6b30;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(664, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // FrmUserMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(269, 129);
-            this.Controls.Add(this.btnUserTick);
-            this.Controls.Add(this.btnLogout);
-            this.Controls.Add(this.btnTicketView);
-            this.Controls.Add(this.btnCreateTick);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtUser);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(181)))), ((int)(((byte)(227)))));
+            this.ClientSize = new System.Drawing.Size(666, 449);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FrmUserMenu";
             this.Text = "FrmUserMenu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -122,5 +175,9 @@
         private System.Windows.Forms.Button btnTicketView;
         private System.Windows.Forms.Button btnLogout;
         private System.Windows.Forms.Button btnUserTick;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
