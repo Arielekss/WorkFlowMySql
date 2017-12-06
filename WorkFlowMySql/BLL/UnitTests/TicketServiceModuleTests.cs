9c231b4e7565b9e7795b3009b8196587e2def16d
Arielekss <ariel.kazmierczak@gmail.com>
2017-12-06 21:03:40 +0100

    Testy Moq

--- WorkFlowMySql/BLL/UnitTests/TicketServiceModuleTests.cs
+++ WorkFlowMySql/BLL/UnitTests/TicketServiceModuleTests.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using WorkFlowMySql.Data;
 using WorkFlowMySql.GUI;
-
 namespace WorkFlowMySql.BLL.UnitTests
 {
     [TestFixture]
@@ -17,18 +16,49 @@ namespace WorkFlowMySql.BLL.UnitTests
         [Test]
         public void UpdateTicketStatusById()
         {
+            /*var workflowrContextMock = new Mock<WorkFlowContext>();
+            workflowrContextMock.Setup(x => x.Ticket.Add(It.IsAny<TicketHeader>())).Returns(Mock);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             //arrange
-            var mockSet = new Mock<DbSet<TicketHeader>>();
-            var mockContext = new Mock<WorkFlowContext>();
-            mockContext.Setup(m => m.Ticket).Returns(mockSet.Object);
-            //act
-            string ticketStatus = "Cancel";
-            var ticketService = new TicketServiceMethods(mockContext.Object);
-            ticketService.UpdateTicketStatusById(It.IsAny<int>(), ticketStatus, It.IsAny<string>());
+            //var mockSet = new Mock<DbSet<TicketHeader>>();
+            //var mockContext = new Mock<WorkFlowContext>();
+
+            var workflowrContextMock = new Mock<WorkFlowContext>();
+            workflowrContextMock.Setup(x => x.Ticket.Add(It.IsAny<TicketHeader>())).Returns(new TicketHeader
+            {
+                TicketId = 1,
+                Header = "EntityTest",
+                RegisterDate = DateTime.Now,
+                UserRegister = "Ariel",
+                ActiveUser = It.IsAny<string>(),
+                ActiveUserId = It.IsAny<int>(),
+                Guid = Guid.NewGuid().ToString()
+            });
+        
+
+        //act
+         /*    string ticketStatus = "Cancel";
+             var ticketService = new TicketServiceMethods(mockContext.Object);
+             ticketService.UpdateTicketStatusById(It.IsAny<int>(), ticketStatus, It.IsAny<string>());
 
 
-            //assert
-            mockSet.Verify(m => m.Add(It.IsAny<TicketHeader>()), Times.Once());
+             //assert
+             mockSet.Verify(m => m.Add(It.IsAny<TicketHeader>()), Times.Once());*/
         }
     }
 }
